c24bb0aa41ccd48822abab67799db12bc806db21
Marvin Wright <github@wrig.de>
2019-02-25 12:44:57 +0100

    scale data in tests

--- tests/testthat/test_multiclass.R
+++ tests/testthat/test_multiclass.R
@@ -1,46 +1,50 @@
 library(neuralnet)
 context("neuralnet_multiclass")
 
+# Scale iris data
+dat <- iris
+dat[, -5] <- scale(dat[, -5])
+
 test_that("Manual multiclass returns correct dimensions", {
-  nn_multi <- neuralnet((Species == "setosa") + (Species == "versicolor") ~ Petal.Length + Petal.Width, iris, 
+  nn_multi <- neuralnet((Species == "setosa") + (Species == "versicolor") ~ Petal.Length + Petal.Width, dat, 
                         hidden = c(2, 3), linear.output = FALSE)
 
   # Skip if not converged
   skip_if(is.null(nn_multi$weights))
   
-  pred_multi <- predict(nn_multi, iris[, c("Petal.Length", "Petal.Width")])
+  pred_multi <- predict(nn_multi, dat[, c("Petal.Length", "Petal.Width")])
   
   expect_length(nn_multi$weights[[1]], 3) # 2 hidden + 1 output layer
   expect_equal(dim(nn_multi$weights[[1]][[1]]), c(3, 2)) # First hidden layer: 3 in (1 bias) x 2 units
   expect_equal(dim(nn_multi$weights[[1]][[2]]), c(3, 3)) # Second hidden layer: 3 in (1 bias) x 3 units
   expect_equal(dim(nn_multi$weights[[1]][[3]]), c(4, 2)) # Output layer: 4 in (1 bias) x 2 units
   
-  expect_equal(dim(pred_multi), c(nrow(iris), 2)) # Predict 2 outputs 
+  expect_equal(dim(pred_multi), c(nrow(dat), 2)) # Predict 2 outputs 
 })
 
 test_that("Auto multiclass returns correct dimensions", {
-  nn_multi <- neuralnet(Species ~ Petal.Length + Petal.Width, iris, hidden = c(2, 3), linear.output = FALSE)
+  nn_multi <- neuralnet(Species ~ Petal.Length + Petal.Width, dat, hidden = c(2, 3), linear.output = FALSE)
   
   # Skip if not converged
   skip_if(is.null(nn_multi$weights))
   
-  pred_multi <- predict(nn_multi, iris[, c("Petal.Length", "Petal.Width")])
+  pred_multi <- predict(nn_multi, dat[, c("Petal.Length", "Petal.Width")])
     
   expect_length(nn_multi$weights[[1]], 3) # 2 hidden + 1 output layer
   expect_equal(dim(nn_multi$weights[[1]][[1]]), c(3, 2)) # First hidden layer: 3 in (1 bias) x 2 units
   expect_equal(dim(nn_multi$weights[[1]][[2]]), c(3, 3)) # Second hidden layer: 3 in (1 bias) x 3 units
   expect_equal(dim(nn_multi$weights[[1]][[3]]), c(4, 3)) # Output layer: 4 in (1 bias) x 3 units
     
-  expect_equal(dim(pred_multi), c(nrow(iris), 3)) # Predict 3 outputs
+  expect_equal(dim(pred_multi), c(nrow(dat), 3)) # Predict 3 outputs
 })
 
 
 test_that("Same results with manual multiclass", {
   set.seed(42)
-  nn_manual <- neuralnet((Species == "setosa") + (Species == "versicolor") + (Species == "virginica") ~ Petal.Length + Petal.Width, iris, linear.output = FALSE)
+  nn_manual <- neuralnet((Species == "setosa") + (Species == "versicolor") + (Species == "virginica") ~ Petal.Length + Petal.Width, dat, linear.output = FALSE)
   
   set.seed(42)
-  nn_auto <- neuralnet(Species ~ Petal.Length + Petal.Width, iris, linear.output = FALSE)
+  nn_auto <- neuralnet(Species ~ Petal.Length + Petal.Width, dat, linear.output = FALSE)
   
   expect_equal(nn_manual$weights[[1]][[1]], nn_auto$weights[[1]][[1]])
   expect_equal(nn_manual$weights[[1]][[2]], nn_auto$weights[[1]][[2]])
--- tests/testthat/test_neuralnet.R
+++ tests/testthat/test_neuralnet.R
@@ -1,14 +1,18 @@
 library(neuralnet)
 context("neuralnet")
 
-nn <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, iris, linear.output = FALSE)
-nn2 <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, iris, hidden = c(3, 2), linear.output = FALSE)
+# Scale iris data
+dat <- iris
+dat[, -5] <- scale(dat[, -5])
+
+nn <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, dat, linear.output = FALSE)
+nn2 <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, dat, hidden = c(3, 2), linear.output = FALSE)
 
 if (!is.null(nn$weights)) {
-  pred <- predict(nn, iris[, c("Petal.Length", "Petal.Width")])
+  pred <- predict(nn, dat[, c("Petal.Length", "Petal.Width")])
 }
 if (!is.null(nn2$weights)) {
-  pred2 <- predict(nn2, iris[, c("Petal.Length", "Petal.Width")])
+  pred2 <- predict(nn2, dat[, c("Petal.Length", "Petal.Width")])
 }
 
 test_that("Fitting returns nn object with correct size", {
@@ -20,44 +24,44 @@ test_that("Fitting returns nn object with correct size", {
 test_that("Prediction returns numeric with correct size", {
   skip_if(is.null(nn$weights))
   expect_is(pred, "matrix")
-  expect_equal(dim(pred), c(nrow(iris), 1))
+  expect_equal(dim(pred), c(nrow(dat), 1))
 })
 
 test_that("predict() function returns list of correct size for unit prediction", {
   skip_if(is.null(nn$weights))
-  pred_all <- predict(nn, iris[, c("Petal.Length", "Petal.Width")], all.units = TRUE)
+  pred_all <- predict(nn, dat[, c("Petal.Length", "Petal.Width")], all.units = TRUE)
   expect_equal(length(pred_all), 3)
 
   skip_if(is.null(nn2$weights))
-  pred_all2 <- predict(nn2, iris[, c("Petal.Length", "Petal.Width")], all.units = TRUE)
+  pred_all2 <- predict(nn2, dat[, c("Petal.Length", "Petal.Width")], all.units = TRUE)
   expect_equal(length(pred_all2), 4)
 })
 
 test_that("predict() works if more variables in data", {
   skip_if(is.null(nn$weights))
-  pred_all <- predict(nn, iris)
-  expect_equal(dim(pred_all), c(nrow(iris), 1))
+  pred_all <- predict(nn, dat)
+  expect_equal(dim(pred_all), c(nrow(dat), 1))
 })
 
 test_that("Custom activation function works", {
-  expect_silent(neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, iris, 
+  expect_silent(neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, dat, 
                           linear.output = FALSE, act.fct = function(x) log(1 + exp(x))))
-  expect_silent(neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, iris, 
+  expect_silent(neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, dat, 
                           linear.output = FALSE, act.fct = function(x) {log(1 + exp(x))}))
   
 })
 
 test_that("Same result with custom activation function", {
   set.seed(10)
-  nn_custom <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, iris, 
+  nn_custom <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, dat, 
                          linear.output = FALSE, act.fct = function(x) 1/(1 + exp(-x)))
   
   set.seed(10)
-  nn_custom2 <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, iris, 
+  nn_custom2 <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, dat, 
                           linear.output = FALSE, act.fct = function(x) {1/(1 + exp(-x))})
   
   set.seed(10)
-  nn_default <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, iris, 
+  nn_default <- neuralnet(Species == "setosa" ~ Petal.Length + Petal.Width, dat, 
                           linear.output = FALSE, act.fct = "logistic")
   
   expect_equal(nn_custom$net.result, nn_custom2$net.result)
@@ -69,16 +73,16 @@ test_that("Same result with custom activation function", {
 
 test_that("Error if 'ce' error function used in non-binary outcome", {
   expect_error(neuralnet(Sepal.Length ~ Petal.Length + Petal.Width, 
-                         iris, linear.output = TRUE, err.fct = "ce"), 
+                         dat, linear.output = TRUE, err.fct = "ce"), 
                "Error function 'ce' only implemented for binary response\\.")
 })
 
 test_that("Dots in formula work", {
   set.seed(42)
-  nn_dot <- neuralnet(Species ~ ., iris, linear.output = FALSE)
+  nn_dot <- neuralnet(Species ~ ., dat, linear.output = FALSE)
   
   set.seed(42)
-  nn_nodot <- neuralnet(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, iris, linear.output = FALSE)
+  nn_nodot <- neuralnet(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, dat, linear.output = FALSE)
   
   expect_equal(nn_dot$weights[[1]][[1]], nn_nodot$weights[[1]][[1]])
   expect_equal(nn_dot$weights[[1]][[2]], nn_nodot$weights[[1]][[2]])
@@ -88,7 +92,7 @@ test_that("Dots in formula work", {
 test_that("No error if replications don't converge", {
   # One fail, one success (2 reps)
   set.seed(1)
-  expect_warning(nn <- neuralnet(Species ~ ., iris, linear.output = FALSE, rep = 2, stepmax = 1e4))
+  expect_warning(nn <- neuralnet(Species ~ .,  iris, linear.output = FALSE, rep = 2, stepmax = 1e4))
   expect_is(nn$net.result, "list")
   expect_length(nn$net.result, 2)
   expect_null(nn$net.result[[1]])
@@ -100,6 +104,6 @@ test_that("No error if replications don't converge", {
 })
 
 test_that("Works with ReLu activation", {
-  expect_silent(neuralnet(Species ~ ., iris, linear.output = FALSE, act.fct = "relu"))
-  expect_silent(neuralnet(Species ~ ., iris, linear.output = FALSE, act.fct = "ReLu"))
+  expect_silent(neuralnet(Species ~ ., dat, linear.output = FALSE, act.fct = "relu"))
+  expect_silent(neuralnet(Species ~ ., dat, linear.output = FALSE, act.fct = "ReLu"))
 })
